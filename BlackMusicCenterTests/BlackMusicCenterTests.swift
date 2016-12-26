f4654c152c6445f74d9e8d2efc63cf55f0c0f3fb
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2016-12-26 22:54:00 +0100

    Add UI Testing Framework

---
+++ BlackMusicCenterTests/BlackMusicCenterTests.swift
@@ -0,0 +1,22 @@
+//
+//  BlackMusicCenterTests.swift
+//  BlackMusicCenterTests
+//
+//  Created by Nicolas Fonsat on 26/12/2016.
+//  Copyright © 2016 Manouana. All rights reserved.
+//
+
+import XCTest
+
+class BlackMusicCenterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}
